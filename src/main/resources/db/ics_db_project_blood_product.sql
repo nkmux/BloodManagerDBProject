4dd14f2862c04e3783684425d77eeb559a1d669d
Khalid Alqhtani <94205606+KhalidAlqhtani@users.noreply.github.com>
2023-12-15 18:37:57 +0300

    DB update

--- src/main/resources/db/ics_db_project_blood_product.sql
+++ src/main/resources/db/ics_db_project_blood_product.sql
@@ -1,6 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `ics_db_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ics_db_project`;
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
 --
--- Host: localhost    Database: ics_db_project
+-- Host: 127.0.0.1    Database: ics_db_project
 -- ------------------------------------------------------
 -- Server version	8.0.35
 
@@ -26,7 +28,7 @@ CREATE TABLE `blood_product` (
   `Blood_id` int NOT NULL,
   `Blood_type` varchar(3) NOT NULL,
   `Expiration_date` date NOT NULL,
-  `Quantity` int NOT NULL,
+  `Quantity` decimal(33,2) NOT NULL,
   PRIMARY KEY (`Blood_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,6 +39,7 @@ CREATE TABLE `blood_product` (
 
 LOCK TABLES `blood_product` WRITE;
 /*!40000 ALTER TABLE `blood_product` DISABLE KEYS */;
+INSERT INTO `blood_product` VALUES (1,'A+','2023-12-27',2.10),(2,'B-','2023-12-22',2.30),(3,'B+','2023-12-27',2.33),(4,'AB+','2023-12-30',1.09),(5,'O-','2024-01-06',2.62);
 /*!40000 ALTER TABLE `blood_product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-15 13:57:00
+-- Dump completed on 2023-12-15 18:34:52
--- src/main/resources/db/ics_db_project_organized_by.sql
+++ src/main/resources/db/ics_db_project_organized_by.sql
@@ -1,6 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `ics_db_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ics_db_project`;
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
 --
--- Host: localhost    Database: ics_db_project
+-- Host: 127.0.0.1    Database: ics_db_project
 -- ------------------------------------------------------
 -- Server version	8.0.35
 
@@ -38,6 +40,7 @@ CREATE TABLE `organized_by` (
 
 LOCK TABLES `organized_by` WRITE;
 /*!40000 ALTER TABLE `organized_by` DISABLE KEYS */;
+INSERT INTO `organized_by` VALUES (3001,2001),(3002,2002);
 /*!40000 ALTER TABLE `organized_by` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-15 13:57:01
+-- Dump completed on 2023-12-15 18:34:52
